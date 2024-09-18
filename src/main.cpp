a4deda6f8f21e22313ba95eacea94df1d879bafd
Teemu Kallio <teemu.kallio@reaktor.com>
2024-09-18 11:01:41 +0300

    define ym registers

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,38 @@
 #define YM_DATA_6 (6) // PA6 pin 28 for Arduino Mega
 #define YM_DATA_7 (7) // PA7 pin 29 for Arduino Mega
 
+// YM2612 registers
+#define YM_REG_LFO (0x22)
+#define YM_REG_DAC (0x2B)
+#define YM_REG_KEY_ON_OFF (0x28)
+
+// Channel 1 registers
+#define YM_REG_CH1_FB_ALG (0xB0)
+#define YM_REG_CH1_OP1_DET_MUL (0x30)
+#define YM_REG_CH1_OP2_DET_MUL (0x34)
+#define YM_REG_CH1_OP3_DET_MUL (0x38)
+#define YM_REG_CH1_OP4_DET_MUL (0x3C)
+#define YM_REG_CH1_OP1_TL (0x40)
+#define YM_REG_CH1_OP2_TL (0x44)
+#define YM_REG_CH1_OP3_TL (0x48)
+#define YM_REG_CH1_OP4_TL (0x4C)
+#define YM_REG_CH1_OP1_RS_AR (0x50)
+#define YM_REG_CH1_OP2_RS_AR (0x54)
+#define YM_REG_CH1_OP3_RS_AR (0x58)
+#define YM_REG_CH1_OP4_RS_AR (0x5C)
+#define YM_REG_CH1_OP1_AM_DR (0x60)
+#define YM_REG_CH1_OP2_AM_DR (0x64)
+#define YM_REG_CH1_OP3_AM_DR (0x68)
+#define YM_REG_CH1_OP4_AM_DR (0x6C)
+#define YM_REG_CH1_OP1_DR2 (0x70)
+#define YM_REG_CH1_OP2_DR2 (0x74)
+#define YM_REG_CH1_OP3_DR2 (0x78)
+#define YM_REG_CH1_OP4_DR2 (0x7C)
+#define YM_REG_CH1_OP1_SL_RR (0x80)
+#define YM_REG_CH1_OP2_SL_RR (0x84)
+#define YM_REG_CH1_OP3_SL_RR (0x88)
+#define YM_REG_CH1_OP4_SL_RR (0x8C)
+
 typedef struct {
 	uint8_t totalLevel;          // 7bit 0-127
 	uint8_t detune;              // 3bit 0-7
@@ -45,69 +77,78 @@ typedef struct {
 	uint8_t frequencyOctave;     // 3bit 0-7
 	uint8_t feedback;            // 3bit 0-7
 	uint8_t algorithm;           // 3bit 0-7
-	uint8_t lfoEnabled;          // 1bit 0-1
-	uint8_t lfoFrequency;        // 3bit 0-7
 	Op ops[4];
 } Channel;
 
-Channel channel1 = {
-	.frequencyLSB = 105,
-	.frequencyMSB = 2,
-	.frequencyOctave = 4,
-	.feedback = 6,
-	.algorithm = 2,
+typedef struct {
+	uint8_t lfoEnabled;          // 1bit 0-1
+	uint8_t lfoFrequency;        // 3bit 0-7
+	Channel channels[6];
+} YM2612;
+
+
+YM2612 ym2612 = {
 	.lfoEnabled = 0,
 	.lfoFrequency = 0,
-	.ops = {
+	.channels = {
 		{
-			.totalLevel = 35,
-			.detune = 7,
-			.multiply = 1,
-			.amplitudeModulation = 0,
-			.secondaryAmplitude = 1,
-			.rateScaling = 2,
-			.attackRate = 31,
-			.decayRate1 = 5,
-			.decayRate2 = 2,
-			.releaseRate = 1,
+			.frequencyLSB = 105,
+			.frequencyMSB = 2,
+			.frequencyOctave = 4,
+			.feedback = 6,
+			.algorithm = 2,
+			.ops = {
+				{
+					.totalLevel = 35,
+					.detune = 7,
+					.multiply = 1,
+					.amplitudeModulation = 0,
+					.secondaryAmplitude = 1,
+					.rateScaling = 2,
+					.attackRate = 31,
+					.decayRate1 = 5,
+					.decayRate2 = 2,
+					.releaseRate = 1,
+				},
+				{
+					.totalLevel = 45,
+					.detune = 0,
+					.multiply = 13,
+					.amplitudeModulation = 0,
+					.secondaryAmplitude = 1,
+					.rateScaling = 2,
+					.attackRate = 25,
+					.decayRate1 = 5,
+					.decayRate2 = 2,
+					.releaseRate = 1,
+				},
+				{
+					.totalLevel = 38,
+					.detune = 3,
+					.multiply = 3,
+					.amplitudeModulation = 0,
+					.secondaryAmplitude = 1,
+					.rateScaling = 1,
+					.attackRate = 31,
+					.decayRate1 = 5,
+					.decayRate2 = 2,
+					.releaseRate = 1,
+				},
+				{
+					.totalLevel = 0,
+					.detune = 0,
+					.multiply = 1,
+					.amplitudeModulation = 0,
+					.secondaryAmplitude = 10,
+					.rateScaling = 2,
+					.attackRate = 20,
+					.decayRate1 = 7,
+					.decayRate2 = 2,
+					.releaseRate = 6,
+				},
+			},
 		},
-		{
-			.totalLevel = 45,
-			.detune = 0,
-			.multiply = 13,
-			.amplitudeModulation = 0,
-			.secondaryAmplitude = 1,
-			.rateScaling = 2,
-			.attackRate = 25,
-			.decayRate1 = 5,
-			.decayRate2 = 2,
-			.releaseRate = 1,
-		},
-		{
-			.totalLevel = 38,
-			.detune = 3,
-			.multiply = 3,
-			.amplitudeModulation = 0,
-			.secondaryAmplitude = 1,
-			.rateScaling = 1,
-			.attackRate = 31,
-			.decayRate1 = 5,
-			.decayRate2 = 2,
-			.releaseRate = 1,
-		},
-		{
-			.totalLevel = 0,
-			.detune = 0,
-			.multiply = 1,
-			.amplitudeModulation = 0,
-			.secondaryAmplitude = 10,
-			.rateScaling = 2,
-			.attackRate = 20,
-			.decayRate1 = 7,
-			.decayRate2 = 2,
-			.releaseRate = 6,
-		},
-	}
+	},
 };
 
 static void write_ym(uint8_t data) {
@@ -164,61 +205,60 @@ void setup() {
 	_delay_ms(10);
 }
 
+
 void loop() {
 	/* === YM2612 Test code === */ 
 	// LFO
-	setreg(0x22, (channel1.lfoEnabled << 3) | channel1.lfoFrequency);
-
-	setreg(0x27, 0x00); // Note off (channel 0)
-	setreg(0x28, 0x01); // Note off (channel 1)
-	setreg(0x28, 0x02); // Note off (channel 2)
-	setreg(0x28, 0x04); // Note off (channel 3)
-	setreg(0x28, 0x05); // Note off (channel 4)
-	setreg(0x28, 0x06); // Note off (channel 5)
+	setreg(YM_REG_LFO, (ym2612.lfoEnabled << 3) | ym2612.lfoFrequency);
 
 	// DAC off
-	setreg(0x2B, 0x00);
+	setreg(YM_REG_DAC, 0x00);
+
+	setreg(0x27, 0x00); // Note off (channel 0)
+	setreg(YM_REG_KEY_ON_OFF, 0x01); // Note off (channel 1)
+	setreg(YM_REG_KEY_ON_OFF, 0x02); // Note off (channel 2)
+	setreg(YM_REG_KEY_ON_OFF, 0x04); // Note off (channel 3)
+	setreg(YM_REG_KEY_ON_OFF, 0x05); // Note off (channel 4)
+	setreg(YM_REG_KEY_ON_OFF, 0x06); // Note off (channel 5)
 
 	// Feedback & algorithm
-	setreg(0xB0, (channel1.feedback << 3) | channel1.algorithm);
+	setreg(YM_REG_CH1_FB_ALG, (ym2612.channels[0].feedback << 3) | ym2612.channels[0].algorithm);
 
 	// Detune & multiply
-	setreg(0x30, (channel1.ops[0].detune << 4) | channel1.ops[0].multiply);
-	setreg(0x34, (channel1.ops[1].detune << 4) | channel1.ops[1].multiply);
-	setreg(0x38, (channel1.ops[2].detune << 4) | channel1.ops[2].multiply);
-	setreg(0x3C, (channel1.ops[3].detune << 4) | channel1.ops[3].multiply);
-
+	setreg(YM_REG_CH1_OP1_DET_MUL, (ym2612.channels[0].ops[0].detune << 4) | ym2612.channels[0].ops[0].multiply);
+	setreg(YM_REG_CH1_OP2_DET_MUL, (ym2612.channels[0].ops[1].detune << 4) | ym2612.channels[0].ops[1].multiply);
+	setreg(YM_REG_CH1_OP3_DET_MUL, (ym2612.channels[0].ops[2].detune << 4) | ym2612.channels[0].ops[2].multiply);
+	setreg(YM_REG_CH1_OP4_DET_MUL, (ym2612.channels[0].ops[3].detune << 4) | ym2612.channels[0].ops[3].multiply);
 
 	// Total level
-	setreg(0x40, channel1.ops[0].totalLevel);
-	setreg(0x44, channel1.ops[1].totalLevel);
-	setreg(0x48, channel1.ops[2].totalLevel);
-	setreg(0x4C, channel1.ops[3].totalLevel);
-
+	setreg(YM_REG_CH1_OP1_TL, ym2612.channels[0].ops[0].totalLevel);
+	setreg(YM_REG_CH1_OP2_TL, ym2612.channels[0].ops[1].totalLevel);
+	setreg(YM_REG_CH1_OP3_TL, ym2612.channels[0].ops[2].totalLevel);
+	setreg(YM_REG_CH1_OP4_TL, ym2612.channels[0].ops[3].totalLevel);
 
 	// Rate scaling & attack rate
-	setreg(0x50, (channel1.ops[0].rateScaling << 6) | channel1.ops[0].attackRate);
-	setreg(0x54, (channel1.ops[1].rateScaling << 6) | channel1.ops[1].attackRate);
-	setreg(0x58, (channel1.ops[2].rateScaling << 6) | channel1.ops[2].attackRate);
-	setreg(0x5C, (channel1.ops[3].rateScaling << 6) | channel1.ops[3].attackRate);
+	setreg(YM_REG_CH1_OP1_RS_AR, (ym2612.channels[0].ops[0].rateScaling << 6) | ym2612.channels[0].ops[0].attackRate);
+	setreg(YM_REG_CH1_OP2_RS_AR, (ym2612.channels[0].ops[1].rateScaling << 6) | ym2612.channels[0].ops[1].attackRate);
+	setreg(YM_REG_CH1_OP3_RS_AR, (ym2612.channels[0].ops[2].rateScaling << 6) | ym2612.channels[0].ops[2].attackRate);
+	setreg(YM_REG_CH1_OP4_RS_AR, (ym2612.channels[0].ops[3].rateScaling << 6) | ym2612.channels[0].ops[3].attackRate);
 
 	// Amplitude modulation & 1st decay rate
-	setreg(0x60, (channel1.ops[0].amplitudeModulation << 7) | channel1.ops[0].decayRate1);
-	setreg(0x64, (channel1.ops[1].amplitudeModulation << 7) | channel1.ops[1].decayRate1);
-	setreg(0x68, (channel1.ops[2].amplitudeModulation << 7) | channel1.ops[2].decayRate1);
-	setreg(0x6C, (channel1.ops[3].amplitudeModulation << 7) | channel1.ops[3].decayRate1);
+	setreg(YM_REG_CH1_OP1_AM_DR, (ym2612.channels[0].ops[0].amplitudeModulation << 7) | ym2612.channels[0].ops[0].decayRate1);
+	setreg(YM_REG_CH1_OP2_AM_DR, (ym2612.channels[0].ops[1].amplitudeModulation << 7) | ym2612.channels[0].ops[1].decayRate1);
+	setreg(YM_REG_CH1_OP3_AM_DR, (ym2612.channels[0].ops[2].amplitudeModulation << 7) | ym2612.channels[0].ops[2].decayRate1);
+	setreg(YM_REG_CH1_OP4_AM_DR, (ym2612.channels[0].ops[3].amplitudeModulation << 7) | ym2612.channels[0].ops[3].decayRate1);
 
 	// 2nd decay rate
-	setreg(0x70, channel1.ops[0].decayRate2);
-	setreg(0x74, channel1.ops[1].decayRate2);
-	setreg(0x78, channel1.ops[2].decayRate2);
-	setreg(0x7C, channel1.ops[3].decayRate2);
+	setreg(YM_REG_CH1_OP1_DR2, ym2612.channels[0].ops[0].decayRate2);
+	setreg(YM_REG_CH1_OP2_DR2, ym2612.channels[0].ops[1].decayRate2);
+	setreg(YM_REG_CH1_OP3_DR2, ym2612.channels[0].ops[2].decayRate2);
+	setreg(YM_REG_CH1_OP4_DR2, ym2612.channels[0].ops[3].decayRate2);
 
 	// Secondary amplitude & release rate
-	setreg(0x80, (channel1.ops[0].secondaryAmplitude << 4) | channel1.ops[0].releaseRate);
-	setreg(0x84, (channel1.ops[1].secondaryAmplitude << 4) | channel1.ops[1].releaseRate);
-	setreg(0x88, (channel1.ops[2].secondaryAmplitude << 4) | channel1.ops[2].releaseRate);
-	setreg(0x8C, (channel1.ops[3].secondaryAmplitude << 4) | channel1.ops[3].releaseRate);
+	setreg(YM_REG_CH1_OP1_SL_RR, (ym2612.channels[0].ops[0].secondaryAmplitude << 4) | ym2612.channels[0].ops[0].releaseRate);
+	setreg(YM_REG_CH1_OP2_SL_RR, (ym2612.channels[0].ops[1].secondaryAmplitude << 4) | ym2612.channels[0].ops[1].releaseRate);
+	setreg(YM_REG_CH1_OP3_SL_RR, (ym2612.channels[0].ops[2].secondaryAmplitude << 4) | ym2612.channels[0].ops[2].releaseRate);
+	setreg(YM_REG_CH1_OP4_SL_RR, (ym2612.channels[0].ops[3].secondaryAmplitude << 4) | ym2612.channels[0].ops[3].releaseRate);
 
 	// Proprietary
 	setreg(0x90, 0x00);
@@ -230,18 +270,18 @@ void loop() {
 	setreg(0xB4, 0xC0);
 
 	// Key off
-	setreg(0x28, 0x00);
+	setreg(YM_REG_KEY_ON_OFF, 0x00);
 
 	// Set frequency
-	setreg(0xA4, (channel1.frequencyOctave << 3) | channel1.frequencyMSB);
-	setreg(0xA0, channel1.frequencyLSB);
+	setreg(0xA4, (ym2612.channels[0].frequencyOctave << 3) | ym2612.channels[0].frequencyMSB);
+	setreg(0xA0, ym2612.channels[0].frequencyLSB);
 
 	while (true) {
 		// cycle tone on/off
 		_delay_ms(200);
-		setreg(0x28, 0xF0); // Key on
+		setreg(YM_REG_KEY_ON_OFF, 0xF0); // Key on
 		_delay_ms(1000);
-		setreg(0x28, 0x00); // Key off
+		setreg(YM_REG_KEY_ON_OFF, 0x00); // Key off
 	}
 }
 
